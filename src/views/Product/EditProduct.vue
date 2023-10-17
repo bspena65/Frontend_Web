eba8a5b87e0a44df16f8db5cec10a6f1ae64253a
bspena65 <bspena65@ucatolica.edu.co>
2023-10-16 19:48:45 -0500

    actualizacion agregar producto

--- src/views/Product/EditProduct.vue
+++ src/views/Product/EditProduct.vue
@@ -33,7 +33,7 @@
             <label>Precio</label>
             <input type="number" class="form-control" v-model="product.price" required>
           </div>
-          <button type="button" class="btn btn-primary" @click="editProduct">Submit</button>
+          <button type="button" class="btn btn-primary" @click="editProduct">Guardar</button>
         </form>
       </div>
       <div class="col-3"></div>
