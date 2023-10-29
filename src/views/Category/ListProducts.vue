08ff772aeb4c1e85dffc3448cf00a830e5ef2c6c
andres-bedoya-u <cabedoya53@ucatolica.edu.co>
2023-10-29 11:55:00 -0500

    Implementacion porducto categoria.

--- src/views/Category/ListProducts.vue
+++ src/views/Category/ListProducts.vue
@@ -22,36 +22,42 @@
 
 <!-- Proceso y llamado componente -->
 <script>
+  import ProductBox from '../../components/Product/ProductBox';
+  export default {
+    name: 'ListProducts',
+    data() {
+      return {
+        id: null,
+        categoryIndex: null,
+        category: {},
+        len: 0,
+        msg: null
+      }
+    },
+    components: { ProductBox },
+    props: ["baseURL", "categories"],
+    mounted() {
+      // Obtener el ID de la categoría desde los parámetros de la ruta
+      this.id = this.$route.params.id;
 
-import ProductBox from '../../components/Product/ProductBox';
-export default {
-  name: 'ListProducts',
-  data() {
-    return {
-      id: null,
-      categoryIndex: null,
-      category: {},
-      len: 0,
-      msg: null
+      // Encontrar el índice de la categoría en el array de categorías
+      this.categoryIndex = this.categories.findIndex(category => category.id == this.id);
+
+      // Obtener la categoría actual
+      this.category = this.categories[this.categoryIndex];
+
+      // Calcular la longitud de los productos en la categoría
+      this.len = this.category.products.length;
+
+      // Establecer el mensaje en base a la cantidad de productos encontrados
+      if (this.len == 0) {
+        this.msg = "Lo sentimos, no se encontraron productos";
+      } else if (this.len == 1) {
+        this.msg = "Sólo se encontró 1 producto";
+      } else {
+        this.msg = this.len + " productos encontrados";
+      }
     }
-  },
-  components: { ProductBox },
-  props: ["baseURL", "categories"],
-  
-  mounted() {
-    // Obtener el ID de la categoría desde los parámetros de la ruta
-    this.id = this.$route.params.id;
-
-    // Encontrar el índice de la categoría en el array de categorías
-    this.categoryIndex = this.categories.findIndex(category => category.id == this.id);
-
-    // Obtener la categoría actual
-    this.category = this.categories[this.categoryIndex];
-
-    // Calcular la longitud de los productos en la categoría
-    this.len = this.category.products.length;
-  }
-
 }
 </script>
 
