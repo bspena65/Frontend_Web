bf62063dfa89e5c1ddb85f012c7742a45a00e737
andres-bedoya-u <cabedoya53@ucatolica.edu.co>
2023-10-30 07:08:22 -0500

    Implementacion falied.

--- src/helper/payment/Failed.vue
+++ src/helper/payment/Failed.vue
@@ -1,17 +1,12 @@
 <template>
+    <!-- Elemento contenedor con centrado de texto -->
     <div class="text-center">
+        <!-- Indicador de carga (spinner) en rojo -->
         <div class="spinner-grow text-danger" role="status">
+            <!-- Texto para lectores de pantalla -->
             <span class="sr-only">Cargando...</span>
         </div>
     </div>
 </template>
 
-<script>
-export default {
-    name: 'FailedPayment',
-    mounted() {
-        localStorage.removeItem("sessionId")
-        window.location.href = '/order'
-    }
-}
-</script>
+
