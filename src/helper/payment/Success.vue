108e5026cb83caced685413e872233e369e586b5
andres-bedoya-u <cabedoya53@ucatolica.edu.co>
2023-10-31 09:10:25 -0500

    Manejo token usuario.

--- src/helper/payment/Success.vue
+++ src/helper/payment/Success.vue
@@ -19,4 +19,24 @@ export default {
       sessionId: null
     }
   },
+  methods: {
+    async saveOrder() {
+      try {
+        // Construir la URL para agregar una orden con el token y la sesión
+        const url = `${this.baseURL}order/add?token=${this.token}&sessionId=${this.sessionId}`;
+        
+        // Realizar una solicitud POST a la URL
+        const response = await axios.post(url);
+
+        // Verificar el código de estado de la respuesta
+        if (response.status === 200 || response.status === 201) {
+          // Redirigir a la ruta '/order' en caso de éxito
+          window.location.href = '/order';
+        } else {
+         
+        }
+      } 
+    }
+  },
 }
+</script>
