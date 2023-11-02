deb1f0b33f64d39f8ec3369723663cf2452870cd
andres-bedoya-u <cabedoya53@ucatolica.edu.co>
2023-11-02 09:27:03 -0500

    diccionario-en completo.

--- src/locales/en.js
+++ src/locales/en.js
@@ -12,7 +12,21 @@ export default {
       signoff: "Sign off",
       placeholder: "¿What are you looking for?",
       title: "Main categories",
-      title1: "Main Products"
+      title1: "Main Products",
+      admin: "Admin",
+      f1: "Get to know us",
+      f2: "about us",
+      f3: "Android application",
+      f4: "iOS app",
+      f5: "Connect with us",
+      f6: "Facebook",
+      f7: "Twitter",
+      f8: "Instagram",  
+      f9: "Let us help you",
+      f10: "Returns Center",
+      f11: "100% purchase protection",
+      f12: "Help",
+      f13: "Application download",
     }
 
   }
