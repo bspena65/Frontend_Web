7a1c340692776bac1b90197b9e0e332867b8efe9
andres-bedoya-u <cabedoya53@ucatolica.edu.co>
2023-11-03 07:04:40 -0500

    enlace router Button adm.

--- src/views/Admin/Admin.vue
+++ src/views/Admin/Admin.vue
@@ -1,21 +1,14 @@
 <template>
   <div class="adminPanel">
+    <!-- Enlace de router para redirigir a la página de administración de categorías -->
     <router-link :to="{ name: 'AdminCategory' }">
       <button type="button" class="btn btn-primary btn-lg">Crear Categorias</button>
     </router-link>
+    
+    <!-- Enlace de router para redirigir a la página de administración de productos -->
     <router-link :to="{ name: 'AdminProduct' }">
       <button type="button" class="btn btn-primary btn-lg">Crear Productos</button>
     </router-link>
   </div>
 </template>
-
-<script>
-export default {
-  mounted() {
-    if (!localStorage.getItem('token')) {
-      this.$router.push({ name: 'Signin' });
-    }
-  }
-}
-</script>
 
