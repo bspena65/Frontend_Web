0617e4ff0a8aeac033f2eb04918188e8c2c121aa
andres-bedoya-u <cabedoya53@ucatolica.edu.co>
2023-11-08 11:42:35 -0500

    Implementacion logica style pagenot.

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -7,3 +7,24 @@
     </h5>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.not-found {
+  text-align: center;
+}
+
+.not-found h2 {
+  font-size: 150px;
+  margin-top: 30px;
+}
+
+.not-found h3 {
+  font-size: 50px;
+}
+</style>
