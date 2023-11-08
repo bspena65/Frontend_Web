7cf624476fa493892677fc93780461020525d0ea
andres-bedoya-u <cabedoya53@ucatolica.edu.co>
2023-11-08 09:27:03 -0500

    template pagina no encontrada.

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -7,24 +7,3 @@
     </h5>
   </div>
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-.not-found {
-  text-align: center;
-}
-
-.not-found h2 {
-  font-size: 150px;
-  margin-top: 30px;
-}
-
-.not-found h3 {
-  font-size: 50px;
-}
-</style>
