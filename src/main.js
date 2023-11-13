4d31b452c63323a9b31424218ddd44746cfb8e76
bspena65 <bspena65@ucatolica.edu.co>
2023-11-12 19:38:41 -0500

    actualizacion proyecto componentes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 
+import swal from 'sweetalert'
+
 //using axios as a global object
 window.axios = require('axios')
 
